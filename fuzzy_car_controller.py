35ab3bd337f7ae22c69be75c5509ddab22e0ffce
Patryk (PsychoX) Ludwikowski <patryk.ludwikowski.7@gmail.com>
2024-01-29 00:41:54 +0100

    Basic driving is working, except that hard turn

--- fuzzy_car_controller.py
+++ fuzzy_car_controller.py
@@ -25,65 +25,60 @@ class FuzzyCarController(CarController):
         velocity = skfuzzy.control.Antecedent(np.arange(0, 200 + 1, 1), 'velocity')
         velocity['SLOW']    = skfuzzy.trapmf(velocity.universe, [0,    0,   0, 150])
         velocity['MEDIUM']  = skfuzzy.trapmf(velocity.universe, [50, 100, 100, 150])
-        velocity['FAST']    = skfuzzy.trapmf(velocity.universe, [     50, 200, 200, 200])
+        velocity['FAST']    = skfuzzy.trapmf(velocity.universe, [50, 200, 200, 200])
 
-        left = skfuzzy.control.Antecedent(np.arange(0, 200 + 1, 1), 'left')
-        left['CLOSE']       = skfuzzy.trapmf(left.universe, [0,   0,   0, 150])
-        left['AWAY']        = skfuzzy.trapmf(left.universe, [    50, 200, 200, 200])
+        balance = skfuzzy.control.Antecedent(np.arange(-200, 200 + 1, 1), 'balance')
+        balance['LEFT']    = skfuzzy.trapmf(balance.universe, [-200, -200, -200, 0])
+        balance['CENTER']  = skfuzzy.trapmf(balance.universe, [-50, 0, 0, 50])
+        balance['RIGHT']   = skfuzzy.trapmf(balance.universe, [0, 200, 200, 200])
 
         head = skfuzzy.control.Antecedent(np.arange(0, 200 + 1, 1), 'head')
         head['CLOSE']       = skfuzzy.trapmf(head.universe, [0,   0,   0, 150])
-        head['AWAY']        = skfuzzy.trapmf(head.universe, [    50, 200, 200, 200])
+        head['AWAY']        = skfuzzy.trapmf(head.universe, [100, 200, 200, 200])
 
-        right = skfuzzy.control.Antecedent(np.arange(0, 200 + 1, 1), 'right')
-        right['CLOSE']      = skfuzzy.trapmf(right.universe, [0,   0,   0, 150])
-        right['AWAY']       = skfuzzy.trapmf(right.universe, [    50, 200, 200, 200])
-
-        self.inputs = [velocity, left, head, right]
+        self.inputs = [velocity, balance, head]
 
     def setup_outputs(self):
-        gas = skfuzzy.control.Consequent(np.arange(0, 1 + 0.05, 0.05), 'gas')
-        gas['NONE']         = skfuzzy.trapmf(gas.universe, [0, 0, 0, 0.25])
-        gas['SOFT']         = skfuzzy.trapmf(gas.universe, [0, 0.5, 0.5, 1])
-        gas['HARD']         = skfuzzy.trapmf(gas.universe, [0.75, 1, 1, 1])
+        gas = skfuzzy.control.Consequent(np.arange(0, 1 + 0.02, 0.02), 'gas')
+        gas['NONE'] = skfuzzy.gaussmf(gas.universe, 0, 0.1)
+        gas['SOFT'] = skfuzzy.gaussmf(gas.universe, 0.5, 0.1)
+        gas['HARD'] = skfuzzy.gaussmf(gas.universe, 1, 0.1)
+        # gas.defuzzify_method = 'som'
 
-        brake = skfuzzy.control.Consequent(np.arange(0, 1 + 0.05, 0.05), 'brake')
-        brake['NONE']       = skfuzzy.trapmf(brake.universe, [0, 0, 0, 0.25])
-        brake['SOFT']       = skfuzzy.trapmf(brake.universe, [0, 0.5, 0.5, 1])
-        brake['HARD']       = skfuzzy.trapmf(brake.universe, [0.75, 1, 1, 1])
+        brake = skfuzzy.control.Consequent(np.arange(0, 1 + 0.02, 0.02), 'brake')
+        brake['NONE'] = skfuzzy.gaussmf(brake.universe, 0, 0.1)
+        brake['SOFT'] = skfuzzy.gaussmf(brake.universe, 0.5, 0.1)
+        brake['HARD'] = skfuzzy.gaussmf(brake.universe, 1, 0.1)
 
         steer = skfuzzy.control.Consequent(np.arange(-1, 1 + 0.05, 0.05), 'steer')
-        steer['RIGHT']      = skfuzzy.trapmf(steer.universe, [-1, 0, 0, 0])
-        steer['NONE']       = skfuzzy.gaussmf(steer.universe, 0, 0.25)
-        steer['LEFT']       = skfuzzy.trapmf(steer.universe, [0, 0, 0, 1])
+        steer['RIGHT'] = skfuzzy.gaussmf(steer.universe, -1, 0.25)
+        steer['NONE']  = skfuzzy.gaussmf(steer.universe, 0, 0.25)
+        steer['LEFT']  = skfuzzy.gaussmf(steer.universe, 1, 0.25)
 
         self.outputs = [gas, brake, steer]
 
     def setup_control_system(self):
         c = skfuzzy.control
-        velocity, left, head, right = self.inputs
+        velocity, balance, head = self.inputs
         gas, brake, steer = self.outputs
 
         self.control_system = c.ControlSystem([
-            c.Rule(left['CLOSE'], steer['RIGHT']),
-            c.Rule(right['CLOSE'], steer['LEFT']),
-            c.Rule(left['CLOSE'] & right['CLOSE'], steer['NONE']),
-            c.Rule(left['AWAY'] & right['AWAY'], steer['NONE']),
-            # c.Rule((left['AWAY'] & right['AWAY']) | (left['AWAY'] & left['AWAY']), steer['NONE']),
+            c.Rule(balance['LEFT'], steer['RIGHT']),
+            c.Rule(balance['RIGHT'], steer['LEFT']),
+            c.Rule(balance['CENTER'], steer['NONE']),
 
             c.Rule(head['CLOSE'], brake['HARD']),
-            c.Rule(velocity['FAST'] & (left['CLOSE'] | right['CLOSE']), brake['SOFT']),
+            c.Rule(velocity['FAST'] & ~balance['CENTER'], brake['SOFT'] % 0.1),
 
-            c.Rule(left['AWAY'] & right['AWAY'] & head['AWAY'], (gas['HARD'], brake['NONE'])),
+            c.Rule(balance['CENTER'] & head['AWAY'], (gas['HARD'], brake['NONE'])),
             c.Rule(head['AWAY'] & velocity['SLOW'], (gas['SOFT'], brake['NONE'])),
             c.Rule(~velocity['SLOW'], gas['NONE']),
         ])
 
     def update_simulation(self, sensors: dict[str, RayCastResult]):
         self.simulation.input['velocity'] = self.car.velocity
-        self.simulation.input['left'] = sensors['left'].distance
+        self.simulation.input['balance'] = sensors['left'].distance - sensors['right'].distance
         self.simulation.input['head'] = sensors['head'].distance
-        self.simulation.input['right'] = sensors['right'].distance
         self.simulation.compute()
         self.gas = self.simulation.output['gas']
         self.brake = self.simulation.output['brake']
@@ -94,7 +89,7 @@ class FuzzyCarController(CarController):
         super().update(dt, *args, **kwargs)
 
     def setup_visualization(self, width: float, height: float):
-        velocity, left, head, right = self.inputs
+        velocity, balance, head = self.inputs
         gas, brake, steer = self.outputs
 
         dpi = 100 # assume it's default
@@ -103,9 +98,8 @@ class FuzzyCarController(CarController):
 
         self.visualizers = [
             MyFuzzyVariableVisualizer(velocity, plt.subplot(gs[0, 2])),
-            MyFuzzyVariableVisualizer(left,     plt.subplot(gs[1, 0])),
+            MyFuzzyVariableVisualizer(balance,  plt.subplot(gs[1, 0])),
             MyFuzzyVariableVisualizer(head,     plt.subplot(gs[1, 1])),
-            MyFuzzyVariableVisualizer(right,    plt.subplot(gs[1, 2])),
             MyFuzzyVariableVisualizer(gas,      plt.subplot(gs[2, 0])),
             MyFuzzyVariableVisualizer(brake,    plt.subplot(gs[2, 1])),
             MyFuzzyVariableVisualizer(steer,    plt.subplot(gs[2, 2])),
